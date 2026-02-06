491a6ed5253bfe330b2da93a28713e48e607f423
Ethan <word.of.ethan@gmail.com>
2026-02-07 03:27:08 +0800

    Update hook-package00.sh

--- scripts/immortalwrt/hook-package00.sh
+++ scripts/immortalwrt/hook-package00.sh
@@ -1,6 +1,161 @@
 #!/bin/bash
 # Set to local prepare
 
+# 颜色输出
+color() {
+    case "$1" in
+        cr) echo -e "\e[1;31m${2}\e[0m" ;;  # 红色
+        cg) echo -e "\e[1;32m${2}\e[0m" ;;  # 绿色
+        cy) echo -e "\e[1;33m${2}\e[0m" ;;  # 黄色
+        cb) echo -e "\e[1;34m${2}\e[0m" ;;  # 蓝色
+        cp) echo -e "\e[1;35m${2}\e[0m" ;;  # 紫色
+        cc) echo -e "\e[1;36m${2}\e[0m" ;;  # 青色
+        cw) echo -e "\e[1;37m${2}\e[0m" ;;  # 白色
+    esac
+}
+
+# 状态显示和时间统计
+status_info() {
+    local task_name="$1" begin_time=$(date +%s) exit_code time_info
+    shift
+    "$@"
+    exit_code=$?
+    [[ "$exit_code" -eq 99 ]] && return 0
+    if [[ -n "$begin_time" ]]; then
+        time_info="==> 用时 $(($(date +%s) - begin_time)) 秒"
+    else
+        time_info=""
+    fi
+    if [[ "$exit_code" -eq 0 ]]; then
+        printf "%s %-52s %s %s %s %s %s %s %s\n" \
+        $(color cy "⏳ $task_name") [ $(color cg ✔) ] $(color cw "$time_info")
+    else
+        printf "%s %-52s %s %s %s %s %s %s %s\n" \
+        $(color cy "⏳ $task_name") [ $(color cr ✖) ] $(color cw "$time_info")
+    fi
+}
+
+# 查找目录
+find_dir() {
+    find $1 -maxdepth 3 -type d -name "$2" -print -quit 2>/dev/null
+}
+
+# 打印信息
+print_info() {
+    printf "%s %-40s %s %s %s\n" "$1" "$2" "$3" "$4" "$5"
+}
+
+# 添加整个源仓库(git clone)
+git_clone() {
+    local repo_url branch target_dir current_dir
+    if [[ "$1" == */* ]]; then
+        repo_url="$1"
+        shift
+    else
+        branch="-b $1 --single-branch"
+        repo_url="$2"
+        shift 2
+    fi
+    target_dir="${1:-${repo_url##*/}}"
+    git clone -q $branch --depth=1 "$repo_url" "$target_dir" 2>/dev/null || {
+        print_info $(color cr 拉取) "$repo_url" [ $(color cr ✖) ]
+        return 1
+    }
+    rm -rf $target_dir/{.git*,README*.md,LICENSE}
+    current_dir=$(find_dir "package/ feeds/ target/" "$target_dir")
+    if [[ -d "$current_dir" ]]; then
+        rm -rf "$current_dir"
+        mv -f "$target_dir" "${current_dir%/*}"
+        print_info $(color cg 替换) "$target_dir" [ $(color cg ✔) ]
+    else
+        mv -f "$target_dir" "$destination_dir"
+        print_info $(color cb 添加) "$target_dir" [ $(color cb ✔) ]
+    fi
+}
+
+# 添加源仓库内的指定目录
+clone_dir() {
+    local repo_url branch temp_dir=$(mktemp -d)
+    if [[ "$1" == */* ]]; then
+        repo_url="$1"
+        shift
+    else
+        branch="-b $1 --single-branch"
+        repo_url="$2"
+        shift 2
+    fi
+    git clone -q $branch --depth=1 "$repo_url" "$temp_dir" 2>/dev/null || {
+        print_info $(color cr 拉取) "$repo_url" [ $(color cr ✖) ]
+        rm -rf "$temp_dir"
+        return 1
+    }
+    local target_dir source_dir current_dir
+    for target_dir in "$@"; do
+        source_dir=$(find_dir "$temp_dir" "$target_dir")
+        [[ -d "$source_dir" ]] || \
+        source_dir=$(find "$temp_dir" -maxdepth 4 -type d -name "$target_dir" -print -quit) && \
+        [[ -d "$source_dir" ]] || {
+            print_info $(color cr 查找) "$target_dir" [ $(color cr ✖) ]
+            continue
+        }
+        current_dir=$(find_dir "package/ feeds/ target/" "$target_dir")
+        if [[ -d "$current_dir" ]]; then
+            rm -rf "$current_dir"
+            mv -f "$source_dir" "${current_dir%/*}"
+            print_info $(color cg 替换) "$target_dir" [ $(color cg ✔) ]
+        else
+            mv -f "$source_dir" "$destination_dir"
+            print_info $(color cb 添加) "$target_dir" [ $(color cb ✔) ]
+        fi
+    done
+    rm -rf "$temp_dir"
+}
+
+# 添加源仓库内的所有子目录
+clone_all() {
+    local repo_url branch temp_dir=$(mktemp -d)
+    if [[ "$1" == */* ]]; then
+        repo_url="$1"
+        shift
+    else
+        branch="-b $1 --single-branch"
+        repo_url="$2"
+        shift 2
+    fi
+    git clone -q $branch --depth=1 "$repo_url" "$temp_dir" 2>/dev/null || {
+        print_info $(color cr 拉取) "$repo_url" [ $(color cr ✖) ]
+        rm -rf "$temp_dir"
+        return 1
+    }
+    process_dir() {
+        while IFS= read -r source_dir; do
+            local target_dir=$(basename "$source_dir")
+            local current_dir=$(find_dir "package/ feeds/ target/" "$target_dir")
+            if [[ -d "$current_dir" ]]; then
+                rm -rf "$current_dir"
+                mv -f "$source_dir" "${current_dir%/*}"
+                print_info $(color cg 替换) "$target_dir" [ $(color cg ✔) ]
+            else
+                mv -f "$source_dir" "$destination_dir"
+                print_info $(color cb 添加) "$target_dir" [ $(color cb ✔) ]
+            fi
+        done < <(find "$1" -maxdepth 1 -mindepth 1 -type d ! -name '.*')
+    }
+    if [[ $# -eq 0 ]]; then
+        process_dir "$temp_dir"
+    else
+        for dir_name in "$@"; do
+            [[ -d "$temp_dir/$dir_name" ]] && process_dir "$temp_dir/$dir_name" || \
+            print_info $(color cr 目录) "$dir_name" [ $(color cr ✖) ]
+        done
+    fi
+    rm -rf "$temp_dir"
+}
+
+# 创建插件保存目录
+destination_dir="ptemp"
+[ -d "$destination_dir" ] || mkdir -p "$destination_dir"
+
 # autocore
 rm -rf package/emortal/autocore
 git clone https://github.com/xuanranran/autocore-arm -b openwrt-25.12 package/emortal/autocore
@@ -40,6 +195,34 @@ sed -i 's/0666/0644/g;s/0777/0755/g' customfeeds/packages/net/samba4/files/smb.c
 # luci-app-sqm
 # rm -rf customfeeds/luci/applications/luci-app-sqm
 # git clone https://git.cooluc.com/sbwml/luci-app-sqm customfeeds/luci/applications/luci-app-sqm
+
+git clone https://github.com/pymumu/luci-app-smartdns customfeeds/luci/applications/luci-app-smartdns
+git clone https://github.com/pymumu/openwrt-smartdns customfeeds/packages/net/smartdns
+
+git clone https://github.com/ximiTech/luci-app-msd_lite customfeeds/luci/applications/luci-app-msd_lite
+git clone https://github.com/ximiTech/msd_lite customfeeds/packages/net/msd_lite
+
+# UU游戏加速器
+github clone https://github.com/coolsnowwolf/luci/tree/master/applications/luci-app-uugamebooster customfeeds/luci/applications/luci-app-uugamebooster
+github clone https://github.com/coolsnowwolf/packages/tree/master/net/uugamebooster customfeeds/packages/net/uugamebooster
+
+# 关机
+git clone https://github.com/sirpdboy/luci-app-poweroffdevice customfeeds/luci/applications/luci-app-poweroffdevice
+
+# v2ray-server
+clone_dir https://github.com/kiddin9/kwrt-packages luci-app-v2ray-server
+mv -f ptemp/luci-app-v2ray-server customfeeds/luci/applications/
+clone_dir https://github.com/kiddin9/kwrt-packages xray-core
+mv -f ptemp/xray-core customfeeds/packages/net/
+
+# 调整 V2ray服务器 到 VPN 菜单 (修正路径)
+sed -i 's/services/vpn/g' customfeeds/luci/applications//luci-app-v2ray-server/luasrc/controller/*.lua
+sed -i 's/services/vpn/g' customfeeds/luci/applications//luci-app-v2ray-server/luasrc/model/cbi/v2ray_server/*.lua
+sed -i 's/services/vpn/g' customfeeds/luci/applications//luci-app-v2ray-server/luasrc/view/v2ray_server/*.htm
+
+# 添加 Turbo ACC 网络加速
+clone_dir https://github.com/kiddin9/kwrt-packages luci-app-turboacc
+mv -f ptemp/luci-app-turboacc customfeeds/luci/applications/
 
 # procps-ng - top
 sed -i 's/enable-skill/enable-skill --disable-modern-top/g' customfeeds/packages/utils/procps-ng/Makefile
