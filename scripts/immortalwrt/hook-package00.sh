d38e3aa431ccf140cfe428babd157cd7e9b1278f
Ethan <word.of.ethan@gmail.com>
2026-02-07 04:27:56 +0800

    Update hook-package00.sh

--- scripts/immortalwrt/hook-package00.sh
+++ scripts/immortalwrt/hook-package00.sh
@@ -196,9 +196,9 @@ sed -i 's/0666/0644/g;s/0777/0755/g' customfeeds/packages/net/samba4/files/smb.c
 # rm -rf customfeeds/luci/applications/luci-app-sqm
 # git clone https://git.cooluc.com/sbwml/luci-app-sqm customfeeds/luci/applications/luci-app-sqm
 
-rm-rf customfeeds/luci/applications/luci-app-smartdns
-rm-rf customfeeds/packages/net/smartdns
-rm-rf customfeeds/packages/net/xray-core
+rm -rf customfeeds/luci/applications/luci-app-smartdns
+rm -rf customfeeds/packages/net/smartdns
+rm -rf customfeeds/packages/net/xray-core
 git clone https://github.com/pymumu/luci-app-smartdns customfeeds/luci/applications/luci-app-smartdns
 git clone https://github.com/pymumu/openwrt-smartdns customfeeds/packages/net/smartdns
 
